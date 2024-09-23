0475742b10c48e57887cb7e39e88aad3ddf3509e
Arshia <arshiamotaghian@yahoo.com>
2024-09-23 10:23:09 +0330

    Add CSS to the register.html

--- Bottle/users_app/admin.py
+++ Bottle/users_app/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
-
-# Register your models here.
+from django.contrib.admin import ModelAdmin,register
+from .models import *
+@register(UserProfile)
+class UserProfileAdmin(ModelAdmin):
+    pass
--- Bottle/users_app/views.py
+++ Bottle/users_app/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .forms import RegisterForm
+from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def Register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("send_bottle")
+            user = form.save()  
+            login(request, user)  
+            messages.success(request, 'Registration successful!')
+            return redirect('home')  
     else:
-        form = RegisterForm()
-    return render(request, "users_app/register.html", {"form": form})
+        form = UserCreationForm()
+    return render(request, 'users_app/register.html', {'form': form})
